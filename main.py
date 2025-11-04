5a995cbf27a20a644b8072067df8fef17274bf6b
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-11-04 18:25:03 +0100

    safe_run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import system
+import subprocess
 
 push = input('Do you want to push it after every modifire?(y/n)(default:y): ').lower()
 if push == '' or push == 'y':
@@ -9,13 +10,19 @@ else:
 init = input('Do you want to init?(y/n)(default:y): ').lower()
 if init == '' or init == 'y':
     system('git init')
+def safe_run(command):
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to execute command: {command}")
+        print(f"Details: {e}")
 
 def main():
     while True:
         change = input("Whats your changes?: ")
-        system('git add .')
-        system(f'git commit -m "{change}"')
+        safe_run('git add .')
+        safe_run(f'git commit -m "{change}"')
         if push:
-            system('git pull')
-            system('git push')
+            safe_run('git pull')
+            safe_run('git push')
 main()
